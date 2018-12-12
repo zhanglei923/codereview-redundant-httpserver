00d18770cf6c7f25f16795885eaea85987c2d5ff
zhanglei <zhang.lei.923@gmail.com>
2018-12-13 02:03:45 +0800

    up

---
+++ loadData.js
@@ -0,0 +1,39 @@
+let eachcontent = require('eachcontent-js')
+let fs = require('fs')
+let pathutil = require('path')
+
+let taskRootPath = pathutil.resolve(__dirname, '../codereview-redundant-tasks')
+let folders = eachcontent.getAllFolders(taskRootPath)
+const thisUtil = {
+    load: (taskId) =>{
+
+        let taskPath = pathutil.resolve(taskRootPath, `./${taskId}`)
+        let tasklist = {}
+        eachcontent.eachContent(taskPath, [/^task/], (txt, path)=>{
+            let pathinfo = pathutil.parse(path);
+            let filename = pathinfo.name;
+            let taskname = filename;
+
+            let arr = txt.split(',');
+            arr.forEach((item)=>{
+                let a = item.split(':')[0];
+                let b = item.split(':')[1];
+                let linenum = b.split('=')[1]
+                b = item.split('=')[0];
+                //console.log(a,b,linenum)
+                if(typeof tasklist[''+linenum] === 'undefined') tasklist[''+linenum] = 0;
+                tasklist[''+linenum]++
+            })
+        })
+        let arr = []
+        for(let linenum in tasklist){
+            arr.push({
+                linenum: parseInt(linenum),
+                count: tasklist[linenum]
+            })
+        }
+        arr.sort().reverse();
+        return arr;
+    }
+}
+module.exports = thisUtil;
--- server.js
+++ server.js
@@ -1,39 +1,20 @@
-let eachcontent = require('eachcontent-js')
 let fs = require('fs')
-let pathutil = require('path')
+const express = require('express')
+let loadData = require('./loadData')
 
-eachcontent.eachContent('D:/workspaces/codereview-redundant-reports/tasks_20181212_031656-871897924093_report',[/./], (str, path)=>{
-    //console.log(str, path)
-})
-let taskRootPath = pathutil.resolve(__dirname, '../codereview-redundant-tasks')
-let folders = eachcontent.getAllFolders(taskRootPath)
+
+
+const app = express()
+const port = 3000
+
+app.get('/load-data', function (req, res) {
+    let arr = loadData.load(taskId)
+    res.json(arr)
+  })
+app.use(express.static('website'))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 let taskId = 'tasks_20181212_031656-871897924093_report'
-let taskPath = pathutil.resolve(taskRootPath, `./${taskId}`)
-let tasklist = {}
-eachcontent.eachContent(taskPath, [/^task/], (txt, path)=>{
-    let pathinfo = pathutil.parse(path);
-    let filename = pathinfo.name;
-    let taskname = filename;
 
-    let arr = txt.split(',');
-    arr.forEach((item)=>{
-        let a = item.split(':')[0];
-        let b = item.split(':')[1];
-        let linenum = b.split('=')[1]
-        b = item.split('=')[0];
-        //console.log(a,b,linenum)
-        if(typeof tasklist[''+linenum] === 'undefined') tasklist[''+linenum] = 0;
-        tasklist[''+linenum]++
-    })
-})
-let arr = []
-for(let linenum in tasklist){
-    arr.push({
-        linenum: parseInt(linenum),
-        count: tasklist[linenum]
-    })
-}
-arr.sort().reverse();
-fs.writeFileSync('./a.json', JSON.stringify(arr))
-console.log(folders)
+// fs.writeFileSync('./a.json', JSON.stringify(arr))
+
